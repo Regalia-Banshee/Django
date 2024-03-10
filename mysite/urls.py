812cdf93c19307f6abd0a3b5ae6f179345edfc59
Regalia-Banshee <siddhurajannair@gmail.com>
2024-03-10 09:05:15 +0530

    Updated views.py and urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from polls.views import welcome
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("something_interesting/",welcome,name="welcome"),
 ]
